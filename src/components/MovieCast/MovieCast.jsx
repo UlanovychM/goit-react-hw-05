f0d29b63ce01bdf4096a25e54334e1d2ebc55e1b
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-04-07 22:43:31 +0300

    update

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,24 +1,60 @@
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { fetchCast } from '../../services/filmApi';
+import { useParams } from 'react-router-dom';
 
-import css from './Contact.module.css';
+import css from './MovieCast.module.css';
 
-const Contact = ({ data: { id, name, number }, onDelete }) => {
+const MovieCast = () => {
+	const [casts, setCasts] = useState([]);
+	const { movieId } = useParams();
+
+	const defaultImg =
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftinypng.com%2F&psig=AOvVaw0k5rakUhG8AltV60G9ih5F&ust=1712604686907000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODq_-frsIUDFQAAAAAdAAAAABAJ';
+
+	useEffect(() => {
+		if (!movieId) return;
+
+		const getData = async () => {
+			try {
+				const data = await fetchCast(movieId);
+				setCasts(data);
+			} catch (e) {
+				console.log(e);
+			}
+		};
+
+		getData();
+	}, [movieId]);
+
 	return (
-		<>
-			<div className={css.container}>
-				<p className={css.name}>{name}</p>
-				<p className={css.number}>{number}</p>
-				<button className={css.btn} onClick={() => onDelete(id)}>
-					Delete
-				</button>
-			</div>
-		</>
+		<div>
+			{cast && (
+				<ul>
+					{casts.map(({ id, character, name, profile_path }) => {
+						<li key={id}>
+							<div>
+								<img
+									src={
+										profile_path
+											? `https://image.tmdb.org/t/p/w500/${profile_path}`
+											: defaultImg
+									}
+									alt='img'
+									width={250}
+								/>
+							</div>
+							<div>
+								<p>{name}</p>
+								<p>
+									Character: <span>{character}</span>
+								</p>
+							</div>
+						</li>;
+					})}
+				</ul>
+			)}
+		</div>
 	);
 };
 
-export default Contact;
-
-Contact.propTypes = {
-	data: PropTypes.object,
-	onDelete: PropTypes.func,
-};
+export default MovieCast;
